12daade44164874c1111379538db00957652d791
王伟 <thehappymouse@gmail.com>
2019-11-29 12:45:26 +0800

    增加 excel

---
+++ common.go
@@ -0,0 +1,67 @@
+package utils
+
+import (
+	"encoding/csv"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"strings"
+)
+
+var p  = message.NewPrinter(language.English)
+
+// 千分位展示
+func GetMoney(v string) string {
+	v2, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		return v
+	}
+	return p.Sprintf("%.2f", v2)
+}
+
+// 读取批定的csv文件
+func LoadCsvData(source string) ([][]string, error) {
+	cntb,err := ioutil.ReadFile(source)
+	if err != nil {
+		return nil, err
+	}
+	r2 := csv.NewReader(strings.NewReader(string(cntb)))
+	return r2.ReadAll()
+}
+// 写入数据到指定的csv文件
+func Write2CsvFile(records [][]string, filename string) error  {
+	nf, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer nf.Close()
+
+	nw := csv.NewWriter(nf)
+	err = nw.WriteAll(records)
+	if err != nil {
+		return err
+	}
+	nw.Flush()
+	return nil
+}
+
+// 读取Excel指定sheet的数据
+func LoadExcel(xlsxFile string, sheet string) [][]string {
+	xlsx, err := excelize.OpenFile(xlsxFile)
+	CheckError(err)
+	rows := xlsx.GetRows(sheet)
+	rows = rows[1:]
+	return rows
+}
+
+func LoadSheet1(xlsxFile string) [][]string {
+
+	xlsx, err := excelize.OpenFile(xlsxFile)
+	CheckError(err)
+	sheet := xlsx.GetSheetName(1)
+	rows := xlsx.GetRows(sheet)
+	return rows
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/thehappymouse/go-utils
 go 1.12
 
 require (
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1
 	github.com/magiconair/properties v1.8.1
 	github.com/rs/zerolog v1.16.0
 	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	golang.org/x/text v0.3.0
 )
